4cf5c788b9ba07da297fb3ead9785379a62ff0c9
sookool99 <shawnmacburnie@gmail.com>
2016-08-28 21:37:54 -0300

    Added attack code

--- hideCode.js
+++ hideCode.js
@@ -11,18 +11,18 @@ for (var i = 0; i < checkpointCount; i++) {
 }
 while (true) {
 
-    for (var i = 0; i < playerCount; i++) {
+    for (var i = 0; i < 2; i++) {
         var inputs = readline().split(' ');
         var x = parseInt(inputs[0]);
         var y = parseInt(inputs[1]);
         var vx = parseInt(inputs[2]);
         var vy = parseInt(inputs[3]);
         var nextCheckPointId = parseInt(inputs[4]);
-        if (i === 0) { // First input is always my racer
-            var myNextCheckPointId = nextCheckPointId; //current checkpoint id
+        if (i === 1) { // First input is always my racer
+            targetX = x;
+            targetY = y;
         }
     }
 
-    CP = checkpoints[myNextCheckPointId];
-    print(CP[0] + ' ' + CP[1] +  ' ' + '100');
+    print(targetX + ' ' + targetY +  ' ' + '100');
 }
