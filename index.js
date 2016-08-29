8f6bef38ef6037ccf6df93ce997202b617351cbf
sookool99 <shawnmacburnie@gmail.com>
2016-08-28 22:44:08 -0300

    added new offset calculations.

--- index.js
+++ index.js
@@ -51,27 +51,41 @@ while (true) {
     CP2 = checkpoints[(myCar.nextCheckPointId + 1) % checkpointCount];
     // calculate new point closest to next checkpoint
     newPoint = pointOnCircle(CP,CP2,550); // 550 is how far from center of CP
-    //  correction terms are myVx, and myVy
-    moveX = newPoint.x - myCar.vx;
-    moveY = newPoint.y - myCar.vy ;
 
-    diffx = CP.x-myCar.x;
-    diffy = CP.y-myCar.y;
+
+    diffx = newPoint.x-myCar.x;
+    diffy = newPoint.y-myCar.y;
+    // distance from current position to CP position
     myDistance = Math.sqrt( ( diffx ) * (diffx) + (diffy) * (diffy) );
-    if (CPCounter == 4 && myDistance > 5000) { // try tweaking 5000 a bit, maybe something smaller
+
+    // Calculate a number between 0-5, where it will be the offset
+    // 1000 = 1
+    // 2000 = 2
+    // 3000 = 4
+    // >=4000 = 5
+    velocityOffset = myDistance / 1000;
+    printErr(velocityOffset)
+    if (velocityOffset > 5) velocityOffset = 5;
+    myCar.vx *= velocityOffset;
+    myCar.vy *= velocityOffset;
+    // try tweaking 5000 a bit, maybe something smaller
+    if (CPCounter == 2 && myDistance > 3000) {
         var thrust = 'BOOST';
     } else {
         var thrust = '100';
     }
 
+    moveX = parseInt(newPoint.x - myCar.vx);
+    moveY = parseInt(newPoint.y - myCar.vy);
+
     //Shoving Tactic
-    if(startCounter == 1 && playerCount == 2) {
-        moveX = enemyCar.x;
-        moveY = enemyCar.y;
-        thrust = 'BOOST';
-    } else if (startCounter == 2) {
-        thrust = 'BOOST';
-    }
+    // if(startCounter == 1 && playerCount == 2) {
+    //     moveX = enemyCar.x;
+    //     moveY = enemyCar.y;
+    //     thrust = 'BOOST';
+    // } else if (startCounter == 2) {
+    //     thrust = 'BOOST';
+    // }
     print(moveX + ' ' + moveY +  ' ' + thrust);
 }
 
